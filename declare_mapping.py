7b6db1a23bd5136bd2dc0f9b395a50951f16e26d
Aditya Purandare <aditya.p1993@hotmail.com>
2016-06-28 15:35:48 -0500

    creates and adds a user

--- declare_mapping.py
+++ declare_mapping.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from connecting import engine
 
 
 Base = declarative_base()
@@ -8,10 +9,13 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    password = Column(String(50))
 
     def __repr__(self):
         return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
-    
+
+Base.metadata.create_all(engine)
+a_user = User(name='Aditya', fullname='Aditya Purandare', password='password')
+print "<User(name='%s', fullname='%s', password='%s')>" % (a_user.name, a_user.fullname, a_user.password)
