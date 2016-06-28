0b4dc27730f2ca1fdc79f0038ef07199eb6f435f
Aditya Purandare <aditya.p1993@hotmail.com>
2016-06-28 16:46:32 -0500

    Add sessions and querying

--- declare_mapping.py
+++ declare_mapping.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from connecting import engine
+from connecting import session, engine
 
 
 Base = declarative_base()
@@ -14,8 +14,34 @@ class User(Base):
     password = Column(String(50))
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+        return "<User(name='%s', fullname='%s', password='%s')>" % \
+               (self.name, self.fullname, self.password)
+    
+def create_tables():
+    Base.metadata.create_all(engine)
 
-Base.metadata.create_all(engine)
-a_user = User(name='Aditya', fullname='Aditya Purandare', password='password')
-print "<User(name='%s', fullname='%s', password='%s')>" % (a_user.name, a_user.fullname, a_user.password)
+def create_users():
+    new_user = User(name='Aditya', fullname='Aditya Purandare', password='password')
+    session.add(new_user)
+    session.add_all([
+        User(name='wendy', fullname='Wendy Williams', password='foobar'),
+        User(name='mary', fullname='Mary Contrary', password='xxg527'),
+        User(name='fred', fullname='Fred Flinstone', password='blah')])
+    new_user.password = 'strongpassword'
+
+def fetch_user():
+    fetched_user = session.query(User).filter_by(name='Aditya').first()
+    print "<User(name='%s', fullname='%s', password='%s')>" % \
+          (fetched_user.name, fetched_user.fullname, fetched_user.password)
+
+def fetch_users():
+    fetched_users = session.query(User).all()
+    for each_user in fetched_users:
+        print "<User(name='%s', fullname='%s', password='%s')>" % \
+              (each_user.name, each_user.fullname, each_user.password)
+
+if __name__ == '__main__':
+    create_tables()
+    create_users()
+    fetch_users()
+    session.commit()
